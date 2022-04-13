5dab73b4a22e53e885ef04417d457b019770007b
Egon92 <90727274+Egon92@users.noreply.github.com>
2022-04-13 13:27:50 +0200

    homepage when not signed in

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,30 @@
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 import "../styling/header.css";
 
 export function Header() {
-    const navigate = useNavigate()
-    return (
-        <section className="header">
-
-        </section>
-    )
+  const navigate = useNavigate();
+  return (
+    <section className="header">
+      <nav className="nav-bar">
+        <div className="left-nav">
+          <div className="logo-wrapper">
+            <img id="logo-pic" src="/public/assets/logo.png" alt="" />
+          </div>
+        </div>
+        <div className="right-nav">
+          <div className="sign-in-header-wrapper">sign in</div>
+          <div className="want-to-hire-header-wrapper">
+            <span id="want-to-hire">| want to hire</span>
+          </div>
+          <div className="want-to-work-header-wrapper">
+            <span id="want-to-work">| want to work</span>
+          </div>
+        </div>
+      </nav>
+      <div className="top-jobs-bar">Top jobs</div>
+      <form className="search-form-wrapper" action="">
+        <input className="search-bar" type="text" placeholder="Search..." />
+      </form>
+    </section>
+  );
 }
