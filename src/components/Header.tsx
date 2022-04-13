856e781c34e0fcb6240eb164289f4f4ab607584f
Gr3g03 <71180086+Gr3g03@users.noreply.github.com>
2022-04-13 14:43:36 +0200

    header done

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,29 +2,33 @@ import { useNavigate } from "react-router-dom";
 import "../styling/header.css";
 
 export function Header() {
-  const navigate = useNavigate();
-  return (
-    <section className="header">
-      <nav className="nav-bar">
-        <div className="left-nav">
-          <div className="logo-wrapper">
-            <img id="logo-pic" src="/public/assets/logo.png" alt="" />
-          </div>
-        </div>
-        <div className="right-nav">
-          <div className="sign-in-header-wrapper">sign in</div>
-          <div className="want-to-hire-header-wrapper">
-            <span id="want-to-hire">| want to hire</span>
-          </div>
-          <div className="want-to-work-header-wrapper">
-            <span id="want-to-work">| want to work</span>
-          </div>
-        </div>
-      </nav>
-      <div className="top-jobs-bar">Top jobs</div>
-      <form className="search-form-wrapper" action="">
-        <input className="search-bar" type="text" placeholder="Search..." />
-      </form>
-    </section>
-  );
+    const navigate = useNavigate();
+    return (
+        <section className="header">
+            <nav className="nav-bar">
+                <div className="left-nav">
+                    <div className="logo-wrapper">
+                        <img id="logo-pic" src={`public/assets/logoworkin.png`} alt="" />
+                    </div>
+                </div>
+                <div className="center-nav">
+                    <form className="search-form-wrapper" action="">
+                        <input className="search-bar" type="text" placeholder="Search..." />
+                        <button className="search_svg"><img src={`public/assets/search.svg`} alt="" /></button>
+                    </form>
+                </div>
+                <div className="right-nav">
+                    <div className="sign-in-header-wrapper">sign in</div>
+                    <div className="want-to-hire-header-wrapper">
+                        <span id="want-to-hire">| want to hire</span>
+                    </div>
+                    <div className="want-to-work-header-wrapper">
+                        <span id="want-to-work">| want to work</span>
+                    </div>
+                </div>
+            </nav>
+            <div className="top-jobs-bar">Top jobs</div>
+
+        </section>
+    );
 }
