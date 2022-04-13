33f7c336cecd5d3a2df1bde5c7d181ca397159ae
Marvin Aliaj <74553145+MarvinRoot@users.noreply.github.com>
2022-04-13 23:14:41 +0200

    small change on App.tsx and header.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,5 @@
 import "./styling/App.css";
 import { Navigate, Route, Routes } from "react-router-dom";
-// import { SignIn } from "./pages/SignIn";
-// import { SignUpAsEmployee } from "./pages/SignUpAsEmployee";
-// import { SignUpAsEmployer } from "./pages/SignUpAsEmployer";
 import { Home } from "./pages/Home";
 import { HomeAsEmployee } from "./pages/HomeAsEmployee";
 import { HomeAsEmployer } from "./pages/HomeAsEmployer";
@@ -13,9 +10,6 @@ function App() {
     <div className="App">
       <Routes>
         <Route index element={<Navigate replace to="/home" />} />
-        {/* <Route path="/sign-in" element={<SignIn />} /> */}
-        {/* <Route path="/sign-up/employee" element={<SignUpAsEmployee />} /> */}
-        {/* <Route path="/sign-up/employer" element={<SignUpAsEmployer />} /> */}
         <Route path="/home" element={<Home />} />
         <Route path="/employee" element={<HomeAsEmployee />} />
         <Route path="/employer" element={<HomeAsEmployer />} />
